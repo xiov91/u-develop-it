fb8b7a1a30b8e4ee74f12d4fa156f6363cfb56d0
xiov91 <99517717+xiov91@users.noreply.github.com>
2022-07-18 15:01:58 -0400

    lesson 2.4

---
+++ server.js
@@ -0,0 +1,62 @@
+const mysql = require('mysql2');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySql usernanme,
+        user: 'root',
+        //Your MySql password
+        password: 'ebonHawk3!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+/* db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+}); */
+
+/* // GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id =1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+}); */
+
+/* // Delete a candidate
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+}); */
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
